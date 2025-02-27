d264c02866618c12adce3e44a0749cc190b1d334
Henry Harvey <Henry.Harvey@prepass.com>
2025-02-27 10:55:50 -0700

    alphabetically sorted names

--- src/constants.ts
+++ src/constants.ts
@@ -1,20 +1,20 @@
 // Available questions
 export const availableQuestions = [
-  "What is your favorite color?",
-  "What was your first pet's name?",
-  "What is your mother's maiden name?",
   "What city were you born in?",
-  "What was your childhood nickname?",
+  "What is your dream vacation destination?",
   "What is your favorite book?",
+  "What is your favorite color?",
+  "What is your favorite food?",
   "What is your favorite movie?",
+  "What is your favorite season?",
+  "What is your favorite sports team?",
+  "What is your mother's maiden name?",
   "What is your spouse's name?",
   "What street did you grow up on?",
-  "What was the name of your first school?",
-  "What is your favorite food?",
-  "What is your dream vacation destination?",
+  "What was your childhood nickname?",
   "What was your first car?",
-  "What is your favorite sports team?",
-  "What is your favorite season?",
+  "What was your first pet's name?",
+  "What was the name of your first school?"
 ];
 
 // Available separators for PUID generation
